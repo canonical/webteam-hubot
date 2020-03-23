c41fc297c56ff4ec5b301894850879eaaa91151c
Thomas Bille <toto@canonical.com>
2020-03-23 09:22:38 +0000

    Add documentation

--- scripts/greetings.js
+++ scripts/greetings.js
@@ -1,3 +1,13 @@
+// Description:
+//   Make greetings great!
+//   IRC listener for greetings words and reply to the sender.
+//
+// Commands:
+//   None
+//
+// Author:
+//   bartaz
+
 module.exports = function(robot) {
     robot.hear(/^(goo+d )?(mo+rning|night)/i, function(res) {
         const user = res.envelope.user.name;
