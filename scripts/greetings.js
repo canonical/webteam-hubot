374485e36a6e825861c3da1d227b0e9715833f79
Bartek Szopka <bartek.szopka@canonical.com>
2020-03-23 07:35:59 +0100

    Ignore case

--- scripts/greetings.js
+++ scripts/greetings.js
@@ -1,5 +1,5 @@
 module.exports = function(robot) {
-    robot.hear(/^(goo+d )?(mo+rning|night)/, function(res) {
+    robot.hear(/^(goo+d )?(mo+rning|night)/i, function(res) {
         const user = res.envelope.user.name;
         res.send(`${res.match[0]} ${user}`);
     });
