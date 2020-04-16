6f2887c7d6087ad48ea02158656adc92ba9352ac
Thomas Bille <toto@canonical.com>
2020-04-16 14:52:31 +0100

    Add support script

---
+++ scripts/support.js
@@ -0,0 +1,57 @@
+var url = require('url');
+var querystring = require('querystring');
+
+var SECRET_KEY = process.env.HUBOT_RELEASE_NOTIFICATION_SECRET;
+
+module.exports = function(robot) {
+    function updateSupport(res, robot, rooms) {
+        const supportTeam = [
+            'carloswu',
+            'toto',
+            'FranCabrera',
+            'joaomartins',
+            'robin',
+        ];
+
+        let date = new Date().getDay();
+
+        let supportHelp = '';
+        if (date > 0 && date<=5) {
+            let support = supportTeam[date-1];
+            supportHelp =  `- Webteam ask ${support} for support.`;
+        }
+
+        let topic = `ubuntu.com folder - http://ubunt.eu/google-docs. We are the web team. We make websites & apps. Mention @webteam for help ${supportHelp}`
+
+        if (rooms) {
+            rooms.forEach(function (room) {
+                robot.adapter.command('TOPIC', room, topic)
+            });
+        } else {
+            res.topic(topic);
+        }
+    }
+
+    robot.respond(/update support/i, function(res) {
+        updateSupport(res)
+    })
+
+    robot.router.post("/hubot/update-support", async function(req, res) {
+        var query = querystring.parse(url.parse(req.url).query);
+        var data = req.body;
+        if (!query.rooms) {
+            res.send("Parameters rooms required");
+            res.end("")
+        }
+
+        var rooms = query.rooms.split(',');
+
+        if (data.secret && data.secret === SECRET_KEY) {
+            await updateSupport(res, robot, rooms);
+        } else {
+            res.send("Invalid secret");
+        }
+
+        return res.end("");
+    });
+};
