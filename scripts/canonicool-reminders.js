28f18dee2d3d0802636912005540908f7fd690c8
Albert Kolozsvari <albertartk@gmail.com>
2022-03-02 13:56:14 +0000

    sadasd

--- scripts/canonicool-reminders.js
+++ scripts/canonicool-reminders.js
@@ -21,37 +21,74 @@ var url = require("url");
 var querystring = require("querystring");
 
 var SECRET_KEY = process.env.HUBOT_RELEASE_NOTIFICATION_SECRET;
+var MATTERMOST_ACCESS_TOKEN = process.env.MATTERMOST_ACCESS_TOKEN;
 
 module.exports = async function (robot) {
   robot.router.post("/hubot/canonicool-reminders", async function (req, res) {
-    robot
-      .http("https://api.sampleapis.com/movies/animation")
+    robot.http("https://api.sampleapis.com/movies/animation")
       .header("Content-Type", "application/json")
       .get()(function (err, _, body) {
-      const data = JSON.parse(body);
-      if (err) {
-        console.log(err);
-      }
-      const person1 = data[0].title;
-      const person2 = data[1].title;
-      const person3 = data[2].title;
+        const data = JSON.parse(body);
+        if (err) {
+          console.log(err);
+        }
+        const person1 = data[0].title;
+        const person2 = data[1].title;
+        const person3 = data[2].title;
 
-      robot.messageRoom(
-        "sukha",
-        `@${person1}, @${person2} and @${person3}: You're up to present at this week's Canonicool. Please add abfjbfrej`
-      );
+        const post_data = JSON.stringify({
+          channel_id: "dewj9q7uk3d8pymujaez9ksyny",
+          message: `@${person1}, @${person2} and @${person3}: You're up to present at this week's Canonicool. Please add abfjbfrej`
+        });
+
+        robot
+          .http("https://chat.canonical.com/api/v4/posts")
+          .header("Content-Type", "application/json")
+          .header("Authorization", `Bearer ${MATTERMOST_ACCESS_TOKEN}`)
+          .post(post_data)(function (err, _, body) {
+            const data = JSON.parse(body);
+
+            ["x", "white_check_mark"].forEach(emoji => {
+              const reaction_data = JSON.stringify({
+                post_id: data["id"],
+                emoji_name: emoji,
+                user_id: data["user_id"]
+              });
+
+              robot
+                .http("https://chat.canonical.com/api/v4/reactions")
+                .header("Content-Type", "application/json")
+                .header("Authorization", `Bearer ${MATTERMOST_ACCESS_TOKEN}`)
+                .post(reaction_data)(function (err, _, body) {
+                  const data = JSON.parse(body);
+                  console.log(data);
+
+                  if (err) console.log(err);
+                });
+            })
+
+            if (err) console.log(err);
+          });
+
+    //     robot.messageRoom(
+    //       "me-and-webbot",
+    //       `@${person1}, @${person2} and @${person3}: You're up to present at this week's Canonicool. Please add abfjbfrej`
+    //     );
     });
-  });
 
-  robot
-    .http(
-      "https://chat.canonical.com/api/v4/channels/qxfj3ebmntf6urwz888p5frhwa/posts"
-    )
-    .header("Content-Type", "application/json")
-    .get()(function (err, _, body) {
-    console.log(body);
-    if (err) {
-      console.log(err);
-    }
+    console.log("token", MATTERMOST_ACCESS_TOKEN);
+    robot
+      .http(
+        "https://chat.canonical.com/api/v4/channels/dewj9q7uk3d8pymujaez9ksyny/posts"
+      )
+      .header("Authorization", `Bearer ${MATTERMOST_ACCESS_TOKEN}`)
+      .header("Content-Type", "application/json")
+      .get()(function (err, _, body) {
+        console.log(body);
+        console.log("I was ehre!");
+        if (err) {
+          console.log(err);
+        }
+      });
   });
 };
