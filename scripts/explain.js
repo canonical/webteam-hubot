20408a725fc4975b209703aa1af6954022b6a067
Amy Lily <amy.lily@canonical.com>
2022-03-03 13:59:34 +0100

    feat(hubot): explain bot

---
+++ scripts/explain.js
@@ -0,0 +1,140 @@
+// Description:
+//   A place to get initial explanation of products or concepts at Canonical that reads from a Google spreadsheet.
+//
+// Dependencies:
+//   google-spreadsheet: ""
+//
+// Configuration:
+//   set HUBOT_SPREADSHEET_ID in environment
+//   set HUBOT_SPREADSHEET_CLIENT_EMAIL in environment
+//   set HUBOT_SPREADSHEET_PRIVATE_KEY in environment
+//   set MATTERMOST_TOKEN_CMD_ACRONYM in environment
+//
+// Commands:
+//   hubot explain <product|concept>: explains the concepts and provides reading resources
+//
+// URLS:
+//   POST /hubot/explain
+//     Follows format suggested here: https://docs.mattermost.com/developer/slash-commands.html
+//     data:
+//       token: Should be similar than MATTERMOST_TOKEN_CMD_ACRONYM
+//       text: explain
+//     response:
+//       {"response_type": "ephemeral", "text": TEXT_POSTED}
+//
+// Note:
+//   The format of the spreadsheet should be:
+//
+//   | Acronym | Definition   | Link                                                                                              |
+//   | PR      | Pull-Request | https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/about-pull-requests |
+//
+// Authors:
+//   tbille
+
+var SPREADSHEET_ID = process.env.HUBOT_SPREADSHEET_ID;
+if (!SPREADSHEET_ID) {
+    console.log("Missing SPREADSHEET_ID in environment");
+}
+
+var CLIENT_EMAIL = process.env.HUBOT_SPREADSHEET_CLIENT_EMAIL;
+if (!CLIENT_EMAIL) {
+    console.log("Missing CLIENT_ID in environment");
+}
+
+var PRIVATE_KEY = process.env.HUBOT_SPREADSHEET_PRIVATE_KEY;
+if (!PRIVATE_KEY) {
+    console.log('Missing PRIVATE_KEY in environment');
+} else {
+    PRIVATE_KEY = PRIVATE_KEY.replace(/\\n/g, '\n');
+}
+
+var CREDS = {
+    client_email: CLIENT_EMAIL,
+    private_key: PRIVATE_KEY
+};
+
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+
+var HTTPS_PROXY = process.env.HTTPS_PROXY;
+if (HTTPS_PROXY) {
+    doc.axios.defaults.proxy = false;
+    const HttpsProxyAgent = require('https-proxy-agent');
+    doc.axios.defaults.httpsAgent = new HttpsProxyAgent(HTTPS_PROXY);
+}
+
+var MATTERMOST_TOKEN_CMD_ACRONYM = process.env.MATTERMOST_TOKEN_CMD_ACRONYM;
+if (!MATTERMOST_TOKEN_CMD_ACRONYM) {
+    console.log("Missing MATTERMOST_TOKEN_CMD_ACRONYM in environment");
+}
+
+async function googleSpreadsheetHandler(explain) {
+    var explain;
+    //console log input value
+    //console.log("explain: ", explain);
+    await doc.useServiceAccountAuth(CREDS);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    const sheet_why = doc.sheetsByIndex[1];
+    var rows = await sheet.getRows();
+    //get 'why' rows from sheet[1]
+    var why_rows= await sheet_why.getRows();
+    var why_rows_length = why_rows.length;
+
+   var random_node = parseInt(Math.floor(Math.random()*(why_rows_length+1)));
+   //console.log('random node:' + random_node);
+
+    var responses = rows.filter(a => a.Explain && a.Explain.toUpperCase().trim() === explain);
+    //find the random element in why_row
+    var why_text = why_rows.find(a => a.rowIndex == random_node);
+    //console.log(why_text);
+    var display_text = why_text.why;
+
+    var text = "";
+    responses.forEach(function (response) {
+        if (text) { text = text; }
+        var link = response.Link ? response.Link : "";
+        var definition = response.Definition ? response.Definition : "";
+        var MM_Channel = response.Contact ? response.Contact: "";
+        var PM = response.PM ? response.PM: "";
+        text = '\n'+ text + '|' + response.Explain + '| ' + definition + '| \n' + '| PM | ' + PM + '| \n' + '| Contact channel | ' + MM_Channel + ' | \n'+ '| Read more | '+ link + '|';
+    });
+
+
+    if (explain == 'WHY') {
+       return display_text;
+    }
+    else if(text) {
+      return text;
+    }
+    else {
+        return `We cannot explain this product/concept yet. Add your explanation [here](https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID})`;
+    }
+}
+
+module.exports = function(robot) {
+    robot.respond(/explain (.*)/, async function(res) {
+        let explain = res.match[1].toUpperCase().trim();
+        let result = await googleSpreadsheetHandler(explain);
+        res.send(result);
+    });
+
+    robot.router.post("/hubot/explain", async function(req, res) {
+        if (MATTERMOST_TOKEN_CMD_ACRONYM != req.body.token) {
+            res.sendStatus(401);
+            return res.end("");
+        }
+
+        let result = `Format: \`/explain <concept>\` eg. \`/explain MAAS\`. Add your own [here](https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID})`;
+        if (req.body.text) {
+            if (req.body.text.trim() != 'help') {
+                robot.logger.info(': ' + req.body.text.toUpperCase().trim());
+                result = await googleSpreadsheetHandler(req.body.text.toUpperCase().trim());
+            }
+        }
+
+        res.setHeader('content-type', 'application/json');
+        res.send(JSON.stringify({"response_type": "ephemeral", "text": result}));
+        return res.end("");
+    });
+};
