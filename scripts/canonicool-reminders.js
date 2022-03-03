0a9458bd972c75c98ffa563cb56ab2099ebebe8a
ClementChaumel <clement.chaumel@gmail.com>
2022-03-03 15:54:17 +0100

    Clean up some stuff

--- scripts/canonicool-reminders.js
+++ scripts/canonicool-reminders.js
@@ -15,7 +15,7 @@
 //       secret: secret for authentication (HUBOT_RELEASE_NOTIFICATION_SECRET)
 //
 // Authors:
-//   bethcollins92 ClementChaumel
+//   bethcollins92 ClementChaumel albertkol mrgnr mtruj013
 
 const axios = require("axios");
 const MATTERMOST_ACCESS_TOKEN = process.env.MATTERMOST_ACCESS_TOKEN;
@@ -27,8 +27,13 @@ const options = {
   },
 };
 
-const googleScriptBaseURL =
-  "https://script.google.com/macros/s/AKfycbx63w1r5LW7PbfeyvEsMYH5B0ob2Mg9v1yr7Uwm1aI15QTAPGkbd74E_RREPCTTfxS3QA/exec?action=";
+const mattermostBaseURL = `https://chat.canonical.com/api/v4`;
+const channelID = "dewj9q7uk3d8pymujaez9ksyny";
+
+const deploymentID =
+  "AKfycbwszM7zt5YoweTwRvXgh-VLKe0L49-1jIE30lkZXAZs85yMAm4Puzt32-zNZkSCT2wKdg";
+
+const googleScriptBaseURL = `https://script.google.com/macros/s/${deploymentID}/exec?action=`;
 
 const rotateURL = googleScriptBaseURL + "rotate";
 const presentersURL = googleScriptBaseURL + "getPresenters";
@@ -43,12 +48,14 @@ module.exports = async function (robot) {
     const presenters = presentersData.data;
 
     const post_data = JSON.stringify({
-      channel_id: "dewj9q7uk3d8pymujaez9ksyny",
-      message: `@${presenters[0]}, @${presenters[1]} and @${presenters[2]}: You're up to present at this week's Canonicool. Please add abfjbfrej`,
+      channel_id: channelID,
+      message: `:canonicalparty: **Canonicool announcement!** :canonicalparty: 
+      **@${presenters[0]}**, **@${presenters[1]}** and **@${presenters[2]}**: You're up to present at this week's Canonicool. 
+      React with :x: if you can't make it, or :white_check_mark: if you can!`,
     });
 
     const postRes = await axios.post(
-      "https://chat.canonical.com/api/v4/posts",
+      `${mattermostBaseURL}/posts`,
       post_data,
       options
     );
@@ -62,23 +69,16 @@ module.exports = async function (robot) {
         user_id: userID,
       });
 
-      axios.post(
-        "https://chat.canonical.com/api/v4/reactions",
-        reaction_data,
-        options
-      );
+      axios.post(`${mattermostBaseURL}/reactions`, reaction_data, options);
     });
   });
   robot.router.post(
     "/hubot/canonicool-reminders-check",
     async function (req, res) {
-      const mattermostPostsURL =
-        "https://chat.canonical.com/api/v4/channels/dewj9q7uk3d8pymujaez9ksyny/posts";
+      const mattermostPostsURL = `${mattermostBaseURL}/channels/${channelID}/posts`;
 
       const postsRes = await axios.get(mattermostPostsURL, options);
       const posts = Object.values(postsRes.data.posts);
-
-      console.log(posts.length);
 
       const announcements = posts.filter((post) =>
         post.message.includes("You're up to present at this week's Canonicool.")
@@ -95,7 +95,6 @@ module.exports = async function (robot) {
       const cancelledUserIds = [];
 
       reactions.forEach((reaction) => {
-        console.log(reaction.emoji_name);
         if (
           reaction.emoji_name === "x" &&
           reaction.user_id !== "q8yjh4wxupnw5jm5qw4omuw8zw"
@@ -108,7 +107,7 @@ module.exports = async function (robot) {
       var userRes;
       for (const userId of cancelledUserIds) {
         userRes = await axios.get(
-          `https://chat.canonical.com/api/v4/users/${userId}`,
+          `${mattermostBaseURL}/users/${userId}`,
           options
         );
         cancelledUserNames.push(userRes.data.username);
@@ -116,12 +115,10 @@ module.exports = async function (robot) {
 
       var presentersData = await axios.get(presentersURL, options);
       var presenters = presentersData.data;
-      console.log(presenters);
 
       for (const userName of cancelledUserNames) {
         if (presenters.includes(userName)) {
           // cancel the user
-          console.log(`Cancelling ${userName}`);
           const cancelRes = await axios.post(
             cancelURL,
             JSON.stringify({
@@ -134,13 +131,13 @@ module.exports = async function (robot) {
           presenters = presentersData.data;
 
           const post_data = JSON.stringify({
-            channel_id: "dewj9q7uk3d8pymujaez9ksyny",
+            channel_id: channelID,
             message: `Oh no! Someone can't present this week. @${presenters[2]} would you be able to present? please react with :white_check_mark: or :x: on the message above. :point_up_2: Thanks!`,
             root_id: mostRecentAnnoucement.id,
           });
 
           const postRes = await axios.post(
-            "https://chat.canonical.com/api/v4/posts",
+            `${mattermostBaseURL}/posts`,
             post_data,
             options
           );
