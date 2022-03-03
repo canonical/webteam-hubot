a023d4399fe064edaa841ae32856c377cb7a2596
ClementChaumel <clement.chaumel@gmail.com>
2022-03-03 13:58:30 +0100

    Add cancellation function

--- scripts/canonicool-reminders.js
+++ scripts/canonicool-reminders.js
@@ -19,25 +19,25 @@
 
 const axios = require("axios");
 const MATTERMOST_ACCESS_TOKEN = process.env.MATTERMOST_ACCESS_TOKEN;
+const options = {
+  followRedirects: true,
+  contentType: "application/json",
+  headers: {
+    Authorization: "Bearer " + MATTERMOST_ACCESS_TOKEN,
+  },
+};
+
+const googleScriptBaseURL =
+  "https://script.google.com/macros/s/AKfycbx63w1r5LW7PbfeyvEsMYH5B0ob2Mg9v1yr7Uwm1aI15QTAPGkbd74E_RREPCTTfxS3QA/exec?action=";
+
+const rotateURL = googleScriptBaseURL + "rotate";
+const presentersURL = googleScriptBaseURL + "getPresenters";
+const cancelURL = googleScriptBaseURL + "cancel";
 
 module.exports = async function (robot) {
   robot.router.post("/hubot/canonicool-reminders", async function (req, res) {
-    const options = {
-      followRedirects: true,
-      contentType: "application/json",
-      headers: {
-        Authorization: "Bearer " + MATTERMOST_ACCESS_TOKEN,
-      },
-    };
-
-    const baseURL =
-      "https://script.google.com/macros/s/AKfycbx63w1r5LW7PbfeyvEsMYH5B0ob2Mg9v1yr7Uwm1aI15QTAPGkbd74E_RREPCTTfxS3QA/exec?action=";
-
-    const rotateURL = baseURL + "rotate";
-    const presentersURL = baseURL + "getPresenters";
-
     // Rotate the presenters and send a message to the channel pinging the new ones.
-    axios.post(rotateURL, null, options);
+    const rotateRes = await axios.post(rotateURL, null, options);
 
     const presentersData = await axios.get(presentersURL, options);
     const presenters = presentersData.data;
@@ -68,68 +68,84 @@ module.exports = async function (robot) {
         options
       );
     });
-
   });
-  robot.router.post("/hubot/canonicool-reminders-check", async function (req, res) {
-    const two_hours_in_ms = 7200000;
-    const since = new Date().valueOf() - two_hours_in_ms;
+  robot.router.post(
+    "/hubot/canonicool-reminders-check",
+    async function (req, res) {
+      const mattermostPostsURL =
+        "https://chat.canonical.com/api/v4/channels/dewj9q7uk3d8pymujaez9ksyny/posts";
 
-    robot
-      .http(
-        `https://chat.canonical.com/api/v4/channels/dewj9q7uk3d8pymujaez9ksyny/posts?since=${since}`
-      )
-      .header("Authorization", `Bearer ${MATTERMOST_ACCESS_TOKEN}`)
-      .header("Content-Type", "application/json")
-      .get()(function (err, _, body) {
-        const posts = JSON.parse(body)["posts"];
-        let latest_canonicool_reminder = null;
+      const postsRes = await axios.get(mattermostPostsURL, options);
+      const posts = Object.values(postsRes.data.posts);
 
-        for (let key in posts) {
-          const post = posts[key];
-          const message = post["message"];
-          if (message.includes("You're up to present at this week's Canonicool.")) {
-            if (latest_canonicool_reminder === null) {
-              latest_canonicool_reminder = post;
-            }
+      console.log(posts.length);
 
-            if (latest_canonicool_reminder["create_at"] < post["create_at"]) {
-              latest_canonicool_reminder = post;
-            }
-          }
-        }
+      const announcements = posts.filter((post) =>
+        post.message.includes("You're up to present at this week's Canonicool.")
+      );
 
-        metadata = latest_canonicool_reminder["metadata"];
-        reactions = metadata["reactions"];
+      announcements.sort((a, b) => {
+        return new Date(a.create_at) - new Date(b.create_at);
+      });
 
-        if (reactions) {
-          users_cancelled = []
-          reactions.forEach(reaction => {
-            if(reaction["emoji_name"] === "x" && reaction["user_id"] !== "q8yjh4wxupnw5jm5qw4omuw8zw") {
-              users_cancelled.push(reaction["user_id"]);
-            }
-          })
+      const mostRecentAnnoucement = announcements[announcements.length - 1];
 
-          if (users_cancelled.lenght > 0) {
-            user_ids_data = JSON.stringify(users_cancelled);
-            robot
-              .http(`https://chat.canonical.com/api/v4/users/ids`)
-              .header("Authorization", `Bearer ${MATTERMOST_ACCESS_TOKEN}`)
-              .header("Content-Type", "application/json")
-              .post(user_ids_data)(function (err, _, body) {
-                const users = JSON.parse(body);
+      const reactions = mostRecentAnnoucement.metadata.reactions;
 
-                users.forEach(user => {
-                  username = user["username"];
+      const cancelledUserIds = [];
 
-                  // call cancel api for this user
-                });
-
-                if (err) {
-                  console.log(err);
-                }
-              });
-          }
+      reactions.forEach((reaction) => {
+        console.log(reaction.emoji_name);
+        if (
+          reaction.emoji_name === "x" &&
+          reaction.user_id !== "q8yjh4wxupnw5jm5qw4omuw8zw"
+        ) {
+          cancelledUserIds.push(reaction.user_id);
         }
       });
-  });
+
+      const cancelledUserNames = [];
+      var userRes;
+      for (const userId of cancelledUserIds) {
+        userRes = await axios.get(
+          `https://chat.canonical.com/api/v4/users/${userId}`,
+          options
+        );
+        cancelledUserNames.push(userRes.data.username);
+      }
+
+      var presentersData = await axios.get(presentersURL, options);
+      var presenters = presentersData.data;
+      console.log(presenters);
+
+      for (const userName of cancelledUserNames) {
+        if (presenters.includes(userName)) {
+          // cancel the user
+          console.log(`Cancelling ${userName}`);
+          const cancelRes = await axios.post(
+            cancelURL,
+            JSON.stringify({
+              name: userName,
+            }),
+            options
+          );
+
+          presentersData = await axios.get(presentersURL, options);
+          presenters = presentersData.data;
+
+          const post_data = JSON.stringify({
+            channel_id: "dewj9q7uk3d8pymujaez9ksyny",
+            message: `Oh no! Someone can't present this week. @${presenters[2]} would you be able to present? please react with :white_check_mark: or :x: on the message above. :point_up_2: Thanks!`,
+            root_id: mostRecentAnnoucement.id,
+          });
+
+          const postRes = await axios.post(
+            "https://chat.canonical.com/api/v4/posts",
+            post_data,
+            options
+          );
+        }
+      }
+    }
+  );
 };
