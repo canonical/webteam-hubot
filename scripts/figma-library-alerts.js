16ae0f7733b099d9094e8b8a181141849d987b2f
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2024-07-12 14:35:13 +0200

    Google drive api fixes

--- scripts/figma-library-alerts.js
+++ scripts/figma-library-alerts.js
@@ -18,12 +18,22 @@ module.exports = function (robot) {
 
     // Check if X-Goog- header is present, so the callback came from the Google Drive Webhook
     if (req.headers["x-goog-channel-id"]) {
-      let resourceId = req.headers["x-goog-channel-id"];
+      let token = req.headers["x-goog-channel-token"];
       let resourceState = req.headers["x-goog-resource-state"];
-      let message = `Hello library maintainers! :) On Google Drive ${resourceId} changed. Action type: ${resourceState}.  Please update the Figma libraries accordingly.`;
+      let changed = reg.headers["x-goog-changed"];
+      let message = `On Google Drive ${token} changed. Action type: ${resourceState} ${
+        changed ? `Change type: ${changed}` : ""
+      }`;
 
       try {
-        robot.messageRoom(room, message);
+        if (resourceState !== "sync") {
+          if (
+            !changed ||
+            (changed !== "parents" && changed !== "permissions")
+          ) {
+            robot.messageRoom(room, message);
+          }
+        }
       } catch (_error) {
         robot.messageRoom(room, "Whoa, I got an error: " + _error);
         console.log(
@@ -36,7 +46,7 @@ module.exports = function (robot) {
     } else {
       // X-Goog- header not present, use default message which will be used in Github Actions
       let data = req.body;
-      let message = `Hello library maintainers! :) ${data["source"]} has changed. Here is what changed${data["change-summary"]}. Please update the Figma libraries accordingly.`;
+      let message = `${data["source"]} has changed. Changes: ${data["change-summary"]}.`;
 
       try {
         robot.messageRoom(room, message);
